36f1ac584dae057af2a8ea531b875d1a8c5d1906
Harold242001 <harold.berru24@gmail.com>
2026-04-14 10:58:14 -0500

    Eliminación microservicio Creditos

--- creditos-service/src/routes/credito.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const creditoController = require('../controllers/credito.controller');
-
-router.get('/', (req, res) => creditoController.obtenerCreditos(req, res));
-router.post('/', (req, res) => creditoController.crearCredito(req, res));
-
-module.exports = router;
